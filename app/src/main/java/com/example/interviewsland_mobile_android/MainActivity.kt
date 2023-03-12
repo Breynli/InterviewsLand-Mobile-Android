29d1d391886ed05a6970116d9870e3635358ac10
Natiq Haciyev <100693634+natiqhaciyef@users.noreply.github.com>
2023-03-12 19:13:02 +0400

    Project config deployment

--- app/src/main/java/com/example/interviewsland_mobile_android/MainActivity.kt
+++ app/src/main/java/com/example/interviewsland_mobile_android/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.interviewsland_mobile_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.interviewsland_mobile_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
